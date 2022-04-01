7d4e83b97b4becd1483a197f9e98a38cbc09d4d5
Brendan Sweet <91926346+bcsweet@users.noreply.github.com>
2022-04-01 11:34:34 -0400

    Update lab2.cpp

--- lab2.cpp
+++ lab2.cpp
@@ -9,18 +9,17 @@ struct data{
 };
 
 int main(int argc, char* argv[]){
-	printf("Hello!\n");
+printf("Hello!\n");
 	vector <struct data> cache;
-    vector <int>set;
+	vector <int>set;
 	int block_size;
 	int num_blocks; //Number of blocks in the cache. 
 	int associativity; //(only direct mapped is needed).
 	int hit_time; //(in cycles)
 	int miss_time;
-    int blocks_per_index;
+	int blocks_per_index;
 	
 	if (argc !=6){
-		if (argc !=6){
 		cerr << "usage: a.out block_size num_blocks associativity hit_time miss_time"<< endl;
 		cerr << "usage: all arguments are integers"<< endl;
 		cerr << "usage: for associativity 1 for direct mapped "<< endl;
@@ -33,10 +32,9 @@ int main(int argc, char* argv[]){
 		hit_time = atoi(argv[4]);
 		miss_time = atoi(argv[5]);
 	} 
-			
-    int total_num_index = num_blocks / block_size; // this should be the number of sets in the cache
+
+	int total_num_index = num_blocks / block_size; // this should be the number of sets in the cache
 	cache.resize(blocks_per_index);
-<<<<<<< HEAD
 	string address;
 	int hits = 0, misses = 0;
 	while (cin >> address){
@@ -45,8 +43,4 @@ int main(int argc, char* argv[]){
 		int fin_address = stoi(address);
 		printf("the int is %d\n", fin_address);
 	}
-	}
 }
-=======
-}
->>>>>>> 6962b5342fc4e1f767725ac2d13ed9d4ef112e5a
