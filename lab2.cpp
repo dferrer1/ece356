bd9a500682b4679ffc74265095c1211e254c34fe
achin2 <achin2@vols.utk.edu>
2022-04-01 11:39:52 -0400

    hex parsing

--- lab2.cpp
+++ lab2.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include <sstream>
 
 using namespace std;
 
@@ -9,9 +10,9 @@ struct data{
 };
 
 int main(int argc, char* argv[]){
-	printf("Hello!\n");
+	printf("Hello! how are you doing?\n");
 	vector <struct data> cache;
-    vector <int>set;
+    vector <int> associative_maps;
 	int block_size;
 	int num_blocks; //Number of blocks in the cache. 
 	int associativity; //(only direct mapped is needed).
@@ -19,12 +20,12 @@ int main(int argc, char* argv[]){
 	int miss_time;
     int blocks_per_index;
 	
-	if (argc !=6){
 		if (argc !=6){
 		cerr << "usage: a.out block_size num_blocks associativity hit_time miss_time"<< endl;
 		cerr << "usage: all arguments are integers"<< endl;
 		cerr << "usage: for associativity 1 for direct mapped "<< endl;
 		cerr << "usage: for associativity 0 for full associative mapped "<< endl;
+		printf("about to exit in disgrace\n");
 		return 1;
 	}else{
 		block_size = atoi(argv[1]);
@@ -33,20 +34,20 @@ int main(int argc, char* argv[]){
 		hit_time = atoi(argv[4]);
 		miss_time = atoi(argv[5]);
 	} 
-			
     int total_num_index = num_blocks / block_size; // this should be the number of sets in the cache
 	cache.resize(blocks_per_index);
-<<<<<<< HEAD
 	string address;
 	int hits = 0, misses = 0;
+	stringstream ss;
+	int fin_address;
 	while (cin >> address){
 		//ignore the 0x
 		address.erase(0,2);
-		int fin_address = stoi(address);
-		printf("the int is %d\n", fin_address);
+		cout << "the string is " << address << "\n";
+		ss << std::hex << address;
+		ss >> fin_address;
+		ss.clear();
+		printf("Address is %d\n", fin_address);
 	}
-	}
+	return 0;
 }
-=======
-}
->>>>>>> 6962b5342fc4e1f767725ac2d13ed9d4ef112e5a
