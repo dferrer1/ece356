9a26cc30b014360b10ad0eccc2cb827167205850
Diego Ferrer <47149079+dferrer1@users.noreply.github.com>
2022-04-01 10:49:19 -0400

    added vector of vectors

--- lab2.cpp
+++ lab2.cpp
@@ -3,29 +3,26 @@
 
 using namespace std;
 
-int find_block(int address);
-
 int main(int argc, char* argv[]){
-	
+	vector <vector>cache;
+    vector <int>set;
 	int block_size;
 	int num_blocks; //Number of blocks in the cache. 
 	int associativity; //(only direct mapped is needed).
 	int hit_time; //(in cycles)
 	int miss_time;
+    int blocks_per_index;
 	
 	if (argc !=6){
 		fprintf(stderr,"usage: ./lab2 block_size num_blocks associativity(d or a) hit_time miss_time\n");
 		return;
 	}else{
-		Block_size = argv[1];
+		block_size = argv[1];
 		num_blocks = argv[2];
 		associativity = argv[3];
 		hit_time = argv[4];
 		miss_time = argv[5];
 	} 
 	
-}
-
-int find_block() {
-    return -1;
+    blocks_per_index = num_blocks / block_size; // this should be the number of blocks in each set
 }
